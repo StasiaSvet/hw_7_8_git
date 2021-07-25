0bc9c148accca7d1cbc2b1c4b9a8c65a11457e6d
StessyT <wildorhid@gmail.com>
2021-07-25 22:48:24 +0300

    New commit after rebase

--- src/test/java/guru/qa/SecondTest.java
+++ src/test/java/guru/qa/SecondTest.java
@@ -2,11 +2,19 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SecondTest {
 
     @Test
-    void secondTest (){
+    void secondTest() {
 
+        open("https://github.com/");
+        $(byName("q")).setValue("selenide").pressEnter();
+        $(byLinkText("selenide/selenide")).click();
 
     }
 }
