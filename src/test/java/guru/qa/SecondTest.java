f82f1233df42133807a1da8b32fb0c8353fc8d2d
StessyT <wildorhid@gmail.com>
2021-07-25 18:26:53 +0300

    Added open Git line

--- src/test/java/guru/qa/SecondTest.java
+++ src/test/java/guru/qa/SecondTest.java
@@ -2,11 +2,14 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class SecondTest {
 
     @Test
-    void secondTest (){
+    void secondTest() {
 
+        open("https://github.com/");
 
     }
 }
